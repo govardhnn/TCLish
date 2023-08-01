b60128df6fac086f0b4ff86e7861f38fd5497d1e
govardhnn <govardhan@DESKTOP-NLI0UAI.localdomain>
2023-08-01 14:52:58 +0530

    exec pwd

--- tcl/file_access.tcl
+++ tcl/file_access.tcl
@@ -1,7 +1,10 @@
 puts "File Access examples"
-puts "commands\tinfo, file normalize, file dirname\n"
+puts "commands\t exec pwd, info, file normalize, file dirname\n"
+set pwd_path [exec pwd]
+puts "pwd_path is 'exec pwd'"
+puts "PWD Path => $pwd_path" 
 set current_directory [info script]
-puts "current_directory [info script]"
+puts "current_directory is '[info script]'"
 puts "Current Directory => $current_directory\n"
 set full_path [file normalize [info script]]
 puts "full_path is 'file normalize [info script]'"
@@ -9,3 +12,10 @@ puts "Full Path => $full_path\n"
 set only_directory [file dirname [file normalize [info script]]]
 puts "only_directory is 'file dirname [file normalize [info script]]'"
 puts "Only Directory => $only_directory\n"
+puts "===========rought work=============="
+#set filesn [list \
+#		[file normalize "$(HOME)/github/TCLish/tcl/file_access.tcl"] \
+#		[file normalize "$(HOME)/github/TCLish/tcl/expr_pr.tcl"]
+#	   ]
+#puts $filesn
+		 
